a3996eb2b568dcff3f324d9157ff1061b4a7ea73
Waios <waios.floros@hotmail.de>
2017-11-06 12:03:55 +0100

    neue klasse angelegt

---
+++ src/de/merit/azubi/Durschnittsverbrauch.java
@@ -0,0 +1,15 @@
+package de.merit.azubi;
+
+public class Durschnittsverbrauch {
+
+    int gefKm = 430;
+    int verbrauchL = 38;
+    int erg = 0;
+
+    erg = verbrauchL / gefKm * 100;
+
+
+
+
+
+}
