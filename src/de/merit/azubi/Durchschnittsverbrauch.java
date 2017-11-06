cc1733a9c80d88d064049ae8caccbccf3885f2dc
Waios <waios.floros@hotmail.de>
2017-11-06 19:12:18 +0100

    nur so

--- src/de/merit/azubi/Durchschnittsverbrauch.java
+++ src/de/merit/azubi/Durchschnittsverbrauch.java
@@ -5,11 +5,12 @@ public class Durchschnittsverbrauch {
     public static void main(String[] args) {
 
         int gefKm = 430;
-        int verbrauchL = 38;
+        double verbrauchL = 38;
 
         double erg = verbrauchL / gefKm * 100;
 
         System.out.println (erg);
+
 
     }
 }
