6077926823e38f9ca0968d8e3a2fa6a043872001
christiane <christiane.roelle@gmx.de>
2017-11-06 12:17:06 +0100

    neue Klasse

--- src/de/merit/azubi/Durschnittsverbrauch.java
+++ src/de/merit/azubi/Durschnittsverbrauch.java
@@ -4,9 +4,8 @@ public class Durschnittsverbrauch {
 
     int gefKm = 430;
     int verbrauchL = 38;
-    int erg = 0;
 
-    erg = verbrauchL / gefKm * 100;
+    double erg = verbrauchL / gefKm * 100;
 
 
 
