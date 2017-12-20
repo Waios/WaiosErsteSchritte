0c082cf31e7ae2534b81644a1815bc5c7962753f
waios <waios.floros@hotmail.de>
2017-12-20 12:22:01 +0100

    erweiterung mit zweitem Konstruktor

--- src/de/merit/azubi/auto/Kfz.java
+++ src/de/merit/azubi/auto/Kfz.java
@@ -1,17 +1,23 @@
 package de.merit.azubi.auto;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kfz {
 
-    Date bauJahr;
-    Date naechsterTuev;
-    Integer kilometerstand;
-    String kennzeichen;
-    String marke;
-    String besitzer;
+    private Calendar bauJahr;
+    private Calendar naechsterTuev;
+    private Integer kilometerstand = 0;
+    private String kennzeichen;
+    private String marke;
+    private String besitzer;
+    private Calendar zulassung = null;
+    private String farbe;
 
-    public Kfz(Date bauJahr, String kennzeichen, String marke, String besitzer) {
+
+    public Kfz(Calendar bauJahr, String kennzeichen, String marke, String besitzer) {
 
         this.bauJahr = bauJahr;
         this.kennzeichen = kennzeichen;
@@ -21,4 +27,66 @@ public class Kfz {
 
     }
 
+    public Kfz(Calendar bauJahr, String kennzeichen, String marke) {
+        this(bauJahr,kennzeichen,marke,"Emily");
+    }
+
+    public int getBaujahr() {
+        return bauJahr.get(Calendar.YEAR);
+    }
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public String getBesitzer() {
+        return besitzer;
+    }
+
+    public String getKennzeichen() {
+        return kennzeichen;
+    }
+
+    public Integer getKm() {
+        return kilometerstand;
+    }
+
+    public void setKm(int km) {
+        this.kilometerstand = km;
+    }
+
+    public void setTuev(int jahr, int monat) {
+        Calendar calendar = new GregorianCalendar(jahr,monat-1, 1);
+        this.naechsterTuev = calendar;
+    }
+
+    public String getTuev() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        return sdf.format(naechsterTuev.getTime());
+
+    }
+
+    public void setZulassung(int jahr, int monat, int tag) {
+        Calendar calendar = new GregorianCalendar(jahr,monat-1,tag);//Calenadar ist 0 basiert.
+        if (zulassung == null) {
+            this.zulassung = calendar;
+            setTuev(jahr+5, monat);
+        }else {
+            System.out.println("Warnung, Zulassung ist schon gesetzt!" + getZulassung());
+        }
+    }
+
+    public String getZulassung() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        return sdf.format(zulassung.getTime());
+
+    }
+
+    public String getfarbe() {
+        return farbe;
+    }
+
+    public void setFarbe(String farbe) {
+        this.farbe = farbe;
+    }
 }
--- src/de/merit/azubi/auto/KfzTest.java
+++ src/de/merit/azubi/auto/KfzTest.java
@@ -2,23 +2,140 @@ package de.merit.azubi.auto;
 
 import org.junit.Test;
 
-import java.util.Date;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class KfzTest {
 
 
     @Test
-    public void initTest(){
-        Kfz einAuto = new Kfz(new Date(),"DA-RR 1024", "Suzuki", "MeRIT");
+    public void initTest() {
+        Kfz emilysAuto = new Kfz(new GregorianCalendar() , "DA-RR 1024", "Suzuki");
+        assertEquals("Emily", emilysAuto.getBesitzer());
+
+        Kfz einAuto = new Kfz(new GregorianCalendar() , "DA-RR 1024", "Suzuki", "MeRIT");
 
         assertNotNull(einAuto);
 
-        Kfz nochEinAuto = new Kfz(new Date(),"DA-RR 2048", "VW", "MeRIT");
+        Kfz nochEinAuto = new Kfz(new GregorianCalendar() , "DA-RR 2048", "VW", "MeRIT");
         assertNotNull(nochEinAuto);
 
         System.out.println(einAuto);
         System.out.println(nochEinAuto);
     }
+
+    @Test
+    public void initWithDateTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+       assertEquals(2010, einAuto.getBaujahr());
+       System.out.println(einAuto);
+
+        assertEquals("Suzuki", einAuto.getMarke());
+        assertEquals("Waios", einAuto.getBesitzer());
+        assertEquals("DA-RR 1024", einAuto.getKennzeichen());
+
+    }
+
+    @Test
+    public void evalKmTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+        assertEquals(new Integer(0), einAuto.getKm());
+
+        einAuto.setKm(99000);
+        assertEquals(new Integer(99000), einAuto.getKm());
+
+
+    }
+    @Test
+    public void evalZulassungTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+        //einAuto.setZulassung("20100606");
+        einAuto.setZulassung(2010, 07, 06);
+        assertEquals("20100706", einAuto.getZulassung());
+
+
+
+    }
+
+    @Test
+    public void evalTuevTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+        einAuto.setTuev(2015, 04);
+        assertEquals("201504", einAuto.getTuev());
+
+
+
+    }
+    @Test
+    public void zulassungInitTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+
+        einAuto.setZulassung(2010, 07, 06);
+        assertEquals("20100706", einAuto.getZulassung());
+
+        einAuto.setZulassung(2010, 10, 05);
+        assertEquals("20100706", einAuto.getZulassung());
+
+        }
+
+    @Test
+    public void zulassungTuevTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+
+        einAuto.setZulassung(2010, 07, 06);
+        assertEquals("20100706", einAuto.getZulassung());
+
+
+        assertEquals("201507", einAuto.getTuev());
+
+    }
+
+
+    @Test
+    public void farbenTest() throws ParseException {
+
+
+        Calendar jahr = new GregorianCalendar(2010,1,1,0,0,0);
+
+        Kfz einAuto = new Kfz(jahr, "DA-RR 1024", "Suzuki", "Waios");
+
+        einAuto.setFarbe("schwarz");
+        assertEquals("schwarz", einAuto.getfarbe());
+
+
+
+
+    }
+
+
 }
